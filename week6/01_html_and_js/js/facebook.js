10a4619503a71bdc88820c39dc50c6db784bb630
corban baxter <corbanb@gmail.com>
2014-10-07 22:14:05 -0600

    simplify the JS

--- week6/01_html_and_js/js/facebook.js
+++ week6/01_html_and_js/js/facebook.js
@@ -4,26 +4,13 @@ $.ajax({
 	type: 'GET',
 	dataType: 'json'
 })
-.done(function(data) {
+.done(function(response) {
 	
-	var pictures = data.me.photos.data;
-	console.log(data.me.photos);
-	addAllImages(pictures)
+	var pictures = response.me.photos.data;
+
+	for(var i = 0; i<pictures.length; i++){
+		var picture = pictures[i].picture;
+		var imgTag = $('<img />', { src: picture }).appendTo('.fb-images');
+	}
 
 });
-
-
-function addAllImages(pictures){
-
-	var picture = pictures[0].picture;
-
-	var imgTag = $('<img />', { src: picture })
-				.appendTo('.fb-images');
-
-	// loop over the pictures
-	// add each picture to the fb-images class
-	// see js/forloop.js for example
-
-
-
-}
