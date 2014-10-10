fb7bd3e41debe2db9f0df6a5690347c7d7d64ba2
corban baxter <corbanb@gmail.com>
2014-10-09 21:52:37 -0600

    adding in new tests for lat long and gmaps

--- week6/01_html_and_js/js/facebook.js
+++ week6/01_html_and_js/js/facebook.js
@@ -9,8 +9,21 @@ $.ajax({
 	var pictures = response.me.photos.data;
 
 	for(var i = 0; i<pictures.length; i++){
-		var picture = pictures[i].picture;
-		var imgTag = $('<img />', { src: picture }).appendTo('.fb-images');
+
+		if ( pictures[i].place ) {
+
+			var lat = pictures[i].place.location.latitude;
+			var lng = pictures[i].place.location.longitude;
+			var mapsImage = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=3&size=400x400";
+			var imgTag = $('<img />', { src: mapsImage }).appendTo('.fb-images');
+
+		}else{
+
+			var picture = pictures[i].picture;
+			var imgTag = $('<img />', { src: picture }).appendTo('.fb-images');
+
+		}
+
 	}
 
 });
